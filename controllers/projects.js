34283e2cc169db27f24805fead2fd138b8e08040
LewBlu <lewis.blundell.development@gmail.com>
2022-09-15 19:06:55 +0100

    Added express-validator for route body validation

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,4 +1,5 @@
 const Project = require('../models/project');
+const { validationResult } = require('express-validator');
 
 // Get a list of all the projects
 exports.getProjects = (req, res, next) => {
@@ -17,6 +18,12 @@ exports.getProjectByID = (req, res, next) => {
 
 // Create a project
 exports.createProject = (req, res, next) => {
+	const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const error = new Error('Validation failed, entered data is incorrect.');
+        error.statusCode = 422;
+        throw error;
+    }
 	Project.create(req.body).then(result => {
 		return res.status(200).json({message: 'Created project successfully.', result: result});
 	}).catch(err => next(err));
--- routes/projects.js
+++ routes/projects.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body } = require('express-validator');
 
 // Require Controllers
 const projectsController = require('../controllers/projects');
@@ -7,6 +8,8 @@ const projectsController = require('../controllers/projects');
 // Project specific routes
 router.get('/', projectsController.getProjects);
 router.get('/:projectID', projectsController.getProjectByID);
-router.post('/create', projectsController.createProject);
+router.post('/create',[
+	body('title').trim().isLength({min: 3})
+], projectsController.createProject);
 
 module.exports = router;
